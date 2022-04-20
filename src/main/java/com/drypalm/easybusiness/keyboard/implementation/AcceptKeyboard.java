c3ceadb5bfd75dd25aafa54d269a4469a38667aa
Nazar Dzhuruk <nazar.dzhuruk@gmail.com>
2022-04-20 21:17:41 +0200

    Change buttons callback

--- src/main/java/com/drypalm/easybusiness/keyboard/implementation/AcceptKeyboard.java
+++ src/main/java/com/drypalm/easybusiness/keyboard/implementation/AcceptKeyboard.java
@@ -2,6 +2,7 @@ package com.drypalm.easybusiness.keyboard.implementation;
 
 import com.drypalm.easybusiness.keyboard.KeyboardType;
 import com.drypalm.easybusiness.keyboard.MainButtons;
+import com.drypalm.easybusiness.saver.SaveCategory;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
@@ -13,14 +14,21 @@ import static com.drypalm.easybusiness.handler.callback.CallbackType.*;
 
 @Component
 public class AcceptKeyboard implements KeyboardService {
+    private static final String ADD = "add:";
 
     @Override
     public InlineKeyboardMarkup getKeyboard(String callback) {
         List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
 
-        buttons.add(ButtonCreator.createButtons(List.of(ACCEPT_ALCOHOL.getCallback().toUpperCase()), callback));
-        buttons.add(ButtonCreator.createButtons(List.of(ACCEPT_FOOD.getCallback().toUpperCase()), callback));
-        buttons.add(ButtonCreator.createButtons(List.of(ACCEPT_SOFT.getCallback().toUpperCase()), callback));
+        buttons.add(ButtonCreator.createButtons(List.of(ACCEPT_ALCOHOL.getCallback().toUpperCase()),
+                ADD + SaveCategory.ALCOHOL));
+
+        buttons.add(ButtonCreator.createButtons(List.of(ACCEPT_FOOD.getCallback().toUpperCase()),
+                ADD + SaveCategory.FOOD));
+
+        buttons.add(ButtonCreator.createButtons(List.of(ACCEPT_SOFT.getCallback().toUpperCase()),
+                ADD + SaveCategory.SOFT));
+
         buttons.add(ButtonCreator.createButtons(List
                 .of(MainButtons.BACK.getButton(), MainButtons.MAIN_MENU.getButton()), callback));
 
