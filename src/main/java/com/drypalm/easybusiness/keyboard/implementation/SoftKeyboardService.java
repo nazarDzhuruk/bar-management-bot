d7e4352cd64d95afdad8f5b711dbeb84c092d3ee
Nazar Dzhuruk <nazar.dzhuruk@gmail.com>
2022-04-20 21:20:18 +0200

    Add service implementation

--- src/main/java/com/drypalm/easybusiness/keyboard/implementation/SoftKeyboardService.java
+++ src/main/java/com/drypalm/easybusiness/keyboard/implementation/SoftKeyboardService.java
@@ -1,4 +1,37 @@
 package com.drypalm.easybusiness.keyboard.implementation;
 
-public class SoftKeyboardService {
+import com.drypalm.easybusiness.keyboard.KeyboardType;
+import com.drypalm.easybusiness.keyboard.MainButtons;
+import com.drypalm.easybusiness.service.StockService;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class SoftKeyboardService implements KeyboardService {
+    private final StockService stockService;
+
+    public SoftKeyboardService(StockService stockService) {
+        this.stockService = stockService;
+    }
+
+    @Override
+    public InlineKeyboardMarkup getKeyboard(String callback) {
+        List<List<InlineKeyboardButton>> buttons = stockService.getMainStock().getSoftDrinkSet().stream()
+                .map(s -> ButtonCreator.createButtons(List.of(s.getName()), String.valueOf(s.getLitre())))
+                .collect(Collectors.toList());
+
+        buttons.add(ButtonCreator.createButtons(List.of(MainButtons.BACK.getButton(),
+                MainButtons.MAIN_MENU.getButton()), callback));
+
+        return InlineKeyboardMarkup.builder().keyboard(buttons).build();
+    }
+
+    @Override
+    public KeyboardType getImplementation() {
+        return KeyboardType.SOFT_CATEGORY;
+    }
 }
