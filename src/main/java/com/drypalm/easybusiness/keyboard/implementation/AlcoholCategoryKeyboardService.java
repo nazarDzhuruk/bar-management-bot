c4a9bb8175032e8d9fb1f20d9616a650756d34c1
Nazar Dzhuruk <nazar.dzhuruk@gmail.com>
2022-04-20 21:21:02 +0200

    Minor changes

--- src/main/java/com/drypalm/easybusiness/keyboard/implementation/AlcoholCategoryKeyboardService.java
+++ src/main/java/com/drypalm/easybusiness/keyboard/implementation/AlcoholCategoryKeyboardService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,6 +17,7 @@ import static com.drypalm.easybusiness.keyboard.MainButtons.MAIN_MENU;
 @Component
 public class AlcoholCategoryKeyboardService implements KeyboardService {
     private final StockService stockService;
+    private static final String ALCOHOL = "type:alcohol";
 
     public AlcoholCategoryKeyboardService(StockService stockService) {
         this.stockService = stockService;
@@ -25,10 +27,11 @@ public class AlcoholCategoryKeyboardService implements KeyboardService {
     public InlineKeyboardMarkup getKeyboard(String callback) {
 
         List<String> alcoholType = stockService.index().get(0)
-                .getAlcoholDrinkSet().stream().map(AlcoholDrink::getType).collect(Collectors.toList());
+                .getAlcoholDrinkSet().stream().map(AlcoholDrink::getType)
+                .sorted(Comparator.naturalOrder()).distinct().collect(Collectors.toList());
 
         List<List<InlineKeyboardButton>> buttons = alcoholType
-                .stream().map(a -> ButtonCreator.createButtons(List.of(a), "type"))
+                .stream().map(a -> ButtonCreator.createButtons(List.of(a), ALCOHOL))
                 .collect(Collectors.toList());
 
         buttons.add(ButtonCreator.createButtons(List.of(BACK.getButton(), MAIN_MENU.getButton()), callback));
